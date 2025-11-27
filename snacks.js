d2a6ee4a7e439c2df030d626fed9524f78de2a43
Steopa2001 <moraru495@gmail.com>
2025-11-27 12:20:13 +0100

    [feat] add average function with Jest test for calculating array mean

--- snacks.js
+++ snacks.js
@@ -1,14 +1,23 @@
 function getInitials(nomeCompleto) {
-  const [nome, cognome] = nomeCompleto.split(" ").filter(str => str !== "");
+  const [nome, cognome] = nomeCompleto.split(" ").filter((str) => str !== "");
   return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
 }
 
 function createSlug(str) {
-    return str.toLowerCase()
+  return str.toLowerCase();
 }
 
+function average(numeri) {
+  return numeri.reduce((acc, numero) => acc + numero, 0) / numeri.length;
+  //   let somma = 0;
+  //   numeri.forEach((numero) => {
+  //     somma = somma + numero;
+  //   });
+  //   return somma / numeri.length;
+}
 
 module.exports = {
-    getInitials,
-    createSlug
-}
+  getInitials,
+  createSlug,
+  average,
+};
--- snacks.test.js
+++ snacks.test.js
@@ -1,5 +1,4 @@
-const {getInitials, createSlug} = require("./snacks.js");
-
+const { getInitials, createSlug, average } = require("./snacks.js");
 
 // 🏆 Snack 1
 // Creare un test che verifichi la seguente descrizione:
@@ -11,11 +10,19 @@ test("La funzione getInitials restituisce le iniziali di un nome completo.", () 
   expect(getInitials("Maria  Verdi")).toBe("M.V.");
 });
 
-
 // 🏆 Snack 2
 // Creare un test che verifichi la seguente descrizione:
 // "La funzione createSlug restituisce una stringa in lowercase."
-test('La funzione createSlug restituisce una stringa in lowercase. ', () => {
-    expect(createSlug('CIAO SONO UNO SVILUPPATORE')).toBe("ciao sono uno sviluppatore")
-})
+test("La funzione createSlug restituisce una stringa in lowercase. ", () => {
+  expect(createSlug("CIAO SONO UNO SVILUPPATORE")).toBe(
+    "ciao sono uno sviluppatore"
+  );
+});
+
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+//  "La funzione average calcola la media aritmetica di un array di numeri."
+test("La funzione average calcola la media aritmetica di un array di numeri. ", () => {
+  expect(average([5, 15])).toBe(10);
+});
 
