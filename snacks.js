d40acc9bb2b3baeccae2fc82c3c2e90241dda378
Steopa2001 <moraru495@gmail.com>
2025-11-27 11:57:20 +0100

    [feat] add getInitials function with Jest tests for full name initials

--- snacks.js
+++ snacks.js
@@ -1,5 +1,9 @@
+function getInitials(nomeCompleto) {
+  const [nome, cognome] = nomeCompleto.split(" ").filter(str => str !== "");
+  return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
+}
 
 
 module.exports = {
-
+    getInitials,
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,4 +1,12 @@
-const {} = require("./snacks.js")
+const {getInitials} = require("./snacks.js");
 
 
 // 🏆 Snack 1
+// Creare un test che verifichi la seguente descrizione:
+// "La funzione getInitials restituisce le iniziali di un nome completo."
+
+test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
+  expect(getInitials("Mario Rossi")).toBe("M.R.");
+  expect(getInitials("luigi bianchi")).toBe("L.B.");
+  expect(getInitials("Maria  Verdi")).toBe("M.V.");
+});
